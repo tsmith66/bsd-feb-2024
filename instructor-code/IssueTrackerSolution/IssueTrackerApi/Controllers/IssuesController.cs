c21e79accf02028edec657951c41af79cac838eb
Jeff Gonzalez <jeff@hypertheory.com>
2024-02-21 16:19:06 -0500

    collection

--- instructor-code/IssueTrackerSolution/IssueTrackerApi/Controllers/IssuesController.cs
+++ instructor-code/IssueTrackerSolution/IssueTrackerApi/Controllers/IssuesController.cs
@@ -1,21 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Marten;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IssueTrackerApi.Controllers;
 
-public class IssuesController : ControllerBase
+public class IssuesController(IDocumentSession session) : ControllerBase
 {
+
 
     [HttpPost("/issues")]
     public async Task<ActionResult> AddIssueAsync([FromBody] IssueRequest request)
     {
-        var response = new IssueResponse(request.Software, request.Description, DateTimeOffset.Now, IssueStatus.Pending);
+        var response = new IssueResponse(Guid.NewGuid(), request.Software,
+            request.Description, DateTimeOffset.Now, IssueStatus.Pending);
+        session.Insert(response);
+        await session.SaveChangesAsync();
         return Ok(response);
+    }
+
+    [HttpGet("/issues")]
+    public async Task<ActionResult> GetAllIssues()
+    {
+        var response = await session.Query<IssueResponse>().ToListAsync();
+
+        return Ok(new IssuesResponseCollection(response));
     }
 }
 
 public record IssueRequest(string Software, string Description);
 
-public record IssueResponse(string Software, string Description, DateTimeOffset Logged, IssueStatus Status);
+public record IssueResponse(
+    Guid Id,
+    string Software, string Description, DateTimeOffset Logged, IssueStatus Status);
 
 
 public enum IssueStatus { Pending };
+
+public record IssuesResponseCollection(IReadOnlyList<IssueResponse> Data);
