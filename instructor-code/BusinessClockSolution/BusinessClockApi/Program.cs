44c8edf2e809d56245f12864d077b7632d9e2187
Jeff Gonzalez <jeff@hypertheory.com>
2024-02-21 10:47:59 -0500

    unit tests done

--- instructor-code/BusinessClockSolution/BusinessClockApi/Program.cs
+++ instructor-code/BusinessClockSolution/BusinessClockApi/Program.cs
@@ -1,10 +1,13 @@
+using BusinessClockApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // Hey, Microsoft, create me a 
+// api with the basic configuration you think would be a good starting place.
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<IProvideTheBusinessClock, StandardBusinessClock>();
+builder.Services.AddSingleton<ISystemTime, SystemTime>();
 
 // Above this line is "internal" configuration stuff.
 var app = builder.Build();
