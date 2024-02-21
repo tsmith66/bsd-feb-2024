645ded0d5dae07e4171013c8276d825a2837e327
Jeff Gonzalez <jeff@hypertheory.com>
2024-02-21 16:02:29 -0500

    ready to log some issues

--- instructor-code/IssueTrackerSolution/IssueTrackerApi/Program.cs
+++ instructor-code/IssueTrackerSolution/IssueTrackerApi/Program.cs
@@ -1,3 +1,5 @@
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("issues")
+        ?? throw new Exception("No Connection String for Issues");
+
+var apiUrl = builder.Configuration.GetValue<string>("api")
+        ?? throw new Exception("No Api Url");
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
